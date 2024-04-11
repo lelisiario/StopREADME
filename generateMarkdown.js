1ba165e562881e899377dce44805f606587fa917
Lauren Elisiario <lauren.elisiario@gmail.com>
2024-04-11 12:59:50 -0700

    Created user questions and generateMarkdown.

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -10,11 +10,54 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
  
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
   return `# ${data.title}
 
+${licenseBadge}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Project Title](#project-title)
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${licenseSection}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For questions or feedback, please contact [${data.username}](https://github.com/${data.username}) or email [${data.email}](${data.email}).
 `;
 }
 
 module.exports = generateMarkdown;
+
--- index.js
+++ index.js
@@ -3,8 +3,40 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown");
-// TODO: Create an array of questions for user input
-const questions = [];
+//Questions for User
+const questions = [
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "repoLink",
+      message: "What is the link to your repo?",
+    },
+    {
+        type: "input",
+        name: "User Story",
+        message: "Provide a User Story here",
+    },
+    {
+        type: "input",
+        name: "Acceptance Criteria",
+        message: "Provide Acceptance Criteria here",
+    },
+    {
+        type: "input",
+        name: "What does your project do?",
+        message: "Provide a description of your project here",
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "Provide installation instructions here",
+    }
+  ];
+  
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
