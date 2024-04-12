375db9740369f0d774b151bb1b0bd5e4cc614ad4
Lauren Elisiario <lauren.elisiario@gmail.com>
2024-04-11 17:19:53 -0700

    Wrote code for license... Need to test.

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,14 +1,40 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to return a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "GNU GPLv3") {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (license === "Apache 2.0") {
+    return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else {
+    return "";
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function to return the license link
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return "[MIT License](https://opensource.org/licenses/MIT)";
+  } else if (license === "GNU GPLv3") {
+    return "[GNU GPLv3 License](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (license === "Apache 2.0") {
+    return "[Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)";
+  } else {
+    return "";
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function to return the license section of README
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `## License
+
+This project is licensed under the terms of the ${license}. For more details, please see the [${license}](#license) section above.`;
+  } else {
+    return "";
+  }
+}
+
  
 // Function to generate markdown for README
 function generateMarkdown(data) {
